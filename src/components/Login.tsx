0c3ec85440797dc5d1081d1a051278832816934c
Shrikant Vishwakarma <64365738+StrixV54@users.noreply.github.com>
2025-05-27 14:35:32 +0530

    mobile responsive

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -26,9 +26,9 @@ const Login = () => {
   ];
 
   return (
-    <div className="relative h-full w-full font-grotesk">
-      <div className="absolute h-full w-full !bg-[url('/Login/background.png')] bg-[position:center_right] md:bg-[position:30%] bg-[size:cover] bg-[repeat:no-repeat] z-10 p-5 after:content-[''] after:absolute after:top-0 after:left-0 after:w-full after:h-full after:shadow-[0px_0px_0px_9999px_rgba(0,0,0,0.68)]"></div>
-      <div className="relative z-20 w-full h-[calc(100vh-6.8vh)] px-4 md:px-14 flex flex-col">
+    <div className="relative h-full w-full font-grotesk bg-[linear-gradient(to_bottom,#000A10_70%,#000_90%)] overflow-hidden">
+      <div className="absolute sm:h-full h-[60vh] w-full !bg-[url('/Login/background.png')] bg-[position:100%_10%] sm:bg-[position:30%] bg-[size:cover] bg-[repeat:no-repeat] z-10 p-5"></div>
+      <div className="relative z-20 w-full h-[calc(100vh-6.8vh)] overflow-y-auto px-4 md:px-14 flex flex-col">
         <div className="flex py-4">
           <div className="flex flex-row gap-2 justify-center items-center text-center  ">
             <Image
@@ -36,44 +36,45 @@ const Login = () => {
               alt="Dexter"
               height={32}
               width={32}
+              className="hidden sm:block"
             />
-            <h3 className="text-[2.27rem] leading-[2.7rem] text-white  font-bold font-grotesk">
+            <h3 className="sm:text-[2.27rem] sm:leading-[2.7rem] text-2xl text-white  font-bold font-grotesk">
               DEXTER
             </h3>
           </div>
         </div>
-        <div className="relative w-full flex-1 flex flex-col space-y-6 md:space-y-10 items-center justify-center">
+        <div className="relative w-full flex-1 flex flex-col sm:pt-0 xs:pt-[30vh] pt-[20vh] space-y-6 md:space-y-10 items-center justify-center">
           <div className="relative flex flex-col space-y-6 w-full">
-            <h1 className="text-start text-primary-100 text-4xl md:text-8xl font-bold">
+            <h1 className="sm:text-start text-center text-primary-100 sm:text-4xl text-6xl md:text-8xl font-bold">
               DEXTER'S LAB
             </h1>
-            <p className="text-[#e4e8ea] text-sm md:text-xl font-medium tracking-widest leading-5">
+            <p className="text-[#e4e8ea] text-sm md:text-xl md:text-start text-center font-medium tracking-widest sm:leading-5">
               The all-in-one platform to elevate your trading and asset
-              <br />
+              <br className="hidden sm:block" />
               management: Dexter provides a powerful set of tools to maximize
-              <br />
+              <br className="hidden sm:block" />
               Defi opportunities on any chain
             </p>
           </div>
-          <div className="relative flex flex-col space-y-5 w-full pt-8">
-            <h1 className="text-start text-[#e4e8ea] text-xl md:text-3xl font-bold">
+          <div className="relative flex flex-col space-y-5 w-full sm:pt-8">
+            <h1 className="sm:text-start text-center text-[#e4e8ea] text-xl md:text-3xl font-bold">
               How it works
             </h1>
-            <div className="relative flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 w-full">
+            <div className="relative flex flex-row sm:flex-nowrap flex-wrap gap-x-3 gap-y-4 md:gap-y-0 md:gap-x-4 w-full">
               {features.map((box, index) => (
                 <div
                   key={index}
-                  className="relative p-0.5 w-full md:w-[240px] h-[156px]  before:absolute before:inset-0 before:p-[1px] before:rounded-3xl before:bg-gradient-to-tl before:from-[#465e64] before:via-[#00f0ff] before:to-[#00cfff] before:-z-10 before:content-[''] after:absolute after:inset-[1px] after:rounded-3xl after:bg-black/40 after:-z-10 after:content-['']"
+                  className="relative p-0.5 w-[48%] h-auto xs:w-[31%] md:w-[240px] xs:mx-0 mx-auto sm:h-[156px] before:absolute before:inset-0 before:p-[1px] before:rounded-3xl before:bg-gradient-to-tl before:from-[#465e64] before:via-[#00f0ff] before:to-[#00cfff] before:-z-10 before:content-[''] after:absolute after:inset-[1px] after:rounded-3xl after:bg-black/40 after:-z-10 after:content-['']"
                 >
-                  <div className="relative w-full h-full bg-[#041820] rounded-3xl items-center justify-start p-5">
-                    <h3 className="text-primary-100 text-lg font-normal mb-2">
+                  <div className="relative w-full h-full bg-[#041820] rounded-3xl items-center justify-start sm:p-5 p-2.5">
+                    <h3 className="text-primary-100 sm:text-lg text-base font-normal mb-2">
                       0{index + 1}
                     </h3>
-                    <h3 className="text-primary-100 text-lg font-bold mb-2">
+                    <h3 className="text-primary-100 sm:text-lg text-xs font-bold mb-2">
                       {box.title}
                     </h3>
                     <p
-                      className="text-[#e4e8ea] font-semibold text-base leading-5"
+                      className="text-[#e4e8ea] font-semibold sm:text-base text-[0.6875rem] leading-5"
                       style={{ whiteSpace: "pre-line" }}
                     >
                       {box.description}
@@ -83,7 +84,7 @@ const Login = () => {
               ))}
             </div>
           </div>
-          <div className="relative w-full flex justify-center md:justify-start">
+          <div className="relative sm:w-full w-[50%] flex justify-center md:justify-start sm:pt-0 pt-3">
             <button
               className="w-full md:w-auto py-4 px-8 md:px-24 bg-primary-100 text-black text-base sm:text-xl text-center font-bold rounded-xl hover:shadow-lg shadow-primary-100/20 transition-all duration-300"
               onClick={handleEnterLab}
